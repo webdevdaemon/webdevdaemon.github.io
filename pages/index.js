f267c9dd8f10cf82ded4a4ceff410764538ab2bc
Charles E. Morgan <webdevdaemon@gmail.com>
2019-10-16 04:00:50 -0700

    get working hero image transitions prototype;

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,55 @@
-import React from 'react'
-import LoopList from '../components/loop-list'
-import Card from '../components/card'
-import Link from 'next/link'
+import React, {useState} from 'react'
 
-import {subtitles, homePageCardLinks} from '../lists'
-import logo from '../public/daemon.svg'
-import PageWrap from '../layouts/page-wrap'
+import Card from '../components/card'
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import Link from 'next/link'
+import LoopList from '../components/loop-list'
+import PageWrap from '../layouts/page-wrap'
+
+import useInterval from '@use-it/interval'
+import daemon from '../public/daemon.svg'
+import headshot from '../public/cm-headshot.jpg'
+
+import {Image} from 'react-bootstrap'
+import {subtitles, homePageCardLinks} from '../lists'
+import {useTransition, animated} from 'react-spring'
+
+const AnimatedImage = animated(Image)
 
 const Home = () => {
+  const [toggle, set] = useState(false)
+
+  const transitions = useTransition(toggle, null, {
+    from: {opacity: 0, position: 'absolute', width: '50vh', maxWidth: 400},
+    enter: {opacity: 1},
+    leave: {opacity: 0},
+  })
+
+  const animationTick = useInterval(() => set(!toggle), 7000)
+
   return (
     <PageWrap title="home">
       <Jumbotron fluid className="hero home">
-        <div className="row">
-          <img src={logo} width={400} style={{margin: '20px auto'}} />
+        <div className="row empty">
+          {transitions.map(({item, key, props}) =>
+            item ? (
+              <AnimatedImage
+                className={`trans-image headshot`}
+                fluid
+                roundedCircle
+                src={headshot}
+                style={props}
+              />
+            ) : (
+              <AnimatedImage
+                className={`trans-image daemon`}
+                fluid
+                roundedCircle
+                src={daemon}
+                style={props}
+              />
+            ),
+          )}
         </div>
         <h2 className="title">Charles Morgan</h2>
         <h5 className="subtitle">
